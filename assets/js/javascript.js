2ec0769e948de3ed8e6a52fc724be94991a20f00
lakisharma <lakisharma2024@gmail.com>
2026-04-20 13:07:12 +0530

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,15 @@
+ let Register = document.getElementById("register");
+        let login = document.getElementById("login");
+        login.style.display = "none"
+
+
+        function Lopen() {
+            if (login.style.display = "block") {
+                register.style.display = "none"
+            }
+        }
+        function Ropen() {
+            if (register.style.display = "block") {
+                login.style.display = "none"
+            }
+        }
